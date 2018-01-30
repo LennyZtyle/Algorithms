faa5b0c7d23391302b3775293c63501f0ce5b535
LennyZtyle <1enny.beltran.s@gmail.com>
2018-01-29 19:16:41 -0500

    Much closer approach to solving AlmosinCreasingSquence.... verify that the problem is understood 1000%.... did development but the increasing sequence does not have to be consecutive, just strictly increasing....

--- Algorithms/src/main/java/AlmostIncreasingSequence.java
+++ Algorithms/src/main/java/AlmostIncreasingSequence.java
@@ -1,36 +1,46 @@
+import java.util.ArrayList;
 
 public class AlmostIncreasingSequence {
 
 	public static void main(String[] args) {
-		int[] sequence = { 1,2,1,2};
+		 int[] sequence = { 1, 2, 1, 3, 4, 5 };
+		//int[] sequence = { 5, 1, 2, 3, 6 };
 		System.out.println(almostIncreasingSequence(sequence));
 	}
 
 	private static boolean almostIncreasingSequence(int[] sequence) {
-		boolean isValid = true;
-		int ref = 0;
-		for (int i = 0; i < sequence.length; i++) {
-			if (i == 0) {
-				ref = sequence[0];
-			} else {
-				if (sequence[i] < ref) {
-					if (isValid) {
-						isValid = false;
-						ref = sequence[i];
-					} else {
-						return false;
-					}
-				} else if (sequence[i] - 1 != ref) {
-					if (isValid) {
-						isValid = false;
-					} else {
-						return isValid;
-					}
-				} else {
-					ref = sequence[i];
-				}
+		ArrayList<Integer> listPositionBreak = new ArrayList<Integer>();
+		for (int i = 0; i < sequence.length - 1; i++) {
+			if (sequence[i] > sequence[i + 1] - 1) {
+				listPositionBreak.add(i);
 			}
 		}
-		return true;
+		Integer firstEntry = listPositionBreak.get(0);
+		int valuePos = sequence[firstEntry];
+		if (listPositionBreak.size() == 1) {
+			if (firstEntry == 0) {
+				return true;
+			} else if (firstEntry == sequence.length - 2) {
+				return true;
+			} else {
+				int valuePosRefernce = sequence[firstEntry + 2];
+				if (valuePos == valuePosRefernce - 1) {
+					return true;
+				} else {
+					return false;
+				}
+
+			}
+		} else if (listPositionBreak.size() == 2) {
+			int valueLastPos = sequence[listPositionBreak.get(1)+1];			
+			if (valuePos == valueLastPos - 1) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 }
